0e39c5badbf26a0be7e03c16891357e173976c0a
Shyam2214 <109864643+Shyam2214@users.noreply.github.com>
2024-04-03 09:36:25 +0530

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,6 @@
+const contactButton = document.getElementById("contact-me");
+const hiddenLink = document.getElementById("hidden-link");
+
+contactButton.addEventListener("click", function() {
+    hiddenLink.click();
+});
